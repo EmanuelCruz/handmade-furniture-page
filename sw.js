c7846420cc1a9ad85e827c4a7b2d19b89ae122b2
EmanuelCruz <emanuelcruz091@gmail.com>
2021-05-14 12:56:23 -0300

    Fix: Elimino comentarios

--- sw.js
+++ sw.js
@@ -1,27 +1,14 @@
 const CACHE_NAME = "STORES_CACHES_2";
 
-//self a referencia a nuestro service workers
-//funcion cuando la services workers se instale
 self.addEventListener("install", function () {
-  //guardar archivos iniciales
-  //caches.open("STORES_CACHES") retorna una promesa
-  //cache es el resultado de la promesa
   caches.open(CACHE_NAME).then(function (cache) {
-    //agregamos archivos
     cache.addAll(["/index.html", "/dist/javascript/bundle.js"]);
   });
 });
 
 self.addEventListener("activate", function (ev) {
-  //evita que la sw deje de ejecutarse mientras se eliminan las caches
-
-  //pasamos una promesa como parametro a waitUntil
-  //para que la sw espere hasta que se ejecute la promesa
   ev.waitUntil(
-    //caches.keys  devuelve una coleccion con los nombre de los caches en forma de promesa
-    //cacheNames es el nombre de la oleccion que devuelve caches.keys
     caches.keys().then(function (cacheNames) {
-      //creamos una promesa que espere a que todos los caches viejos se eliminen
       let promises = cacheNames.map((cacheName) => {
         if (CACHE_NAME !== cacheName) return caches.delete(cacheName);
       });
@@ -62,5 +49,3 @@ self.addEventListener("fetch", function (ev) {
       })
   );
 });
-
-
